8667afaf307eec42e61200ed25b51d7469e7b43c
phj1120 <hyeonj1998@naver.com>
2023-01-16 23:39:56 +0900

    feat(todo): RestControllerAdvice 등록

---
+++ Todo/Backend/src/main/java/org/parkh/b1/common/controller/exceptions/CommonExceptionHandler.java
@@ -0,0 +1,25 @@
+package org.parkh.b1.common.controller.exceptions;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+@Log4j2
+public class CommonExceptionHandler {
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public ResponseEntity<Map<String, String>> httpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException exception) {
+        log.error("[HttpMediaTypeNotSupportedException]");
+        log.error(exception);
+        log.error("-------------------------------------");
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                .body(Map.of("exception", exception.getMessage()));
+    }
+}
