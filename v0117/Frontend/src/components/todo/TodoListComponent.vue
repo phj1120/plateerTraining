983137413b1d2a0b5351010ef89ce7f43e86c882
phj1120 <hyeonj1998@naver.com>
2023-01-18 06:57:35 +0900

    feat(v0117): 좀 더 이해하기 쉽게 emit 사용

--- v0117/Frontend/src/components/todo/TodoListComponent.vue
+++ v0117/Frontend/src/components/todo/TodoListComponent.vue
@@ -4,7 +4,7 @@
 
   <div>
     <ul>
-      <li v-for="todo in data.dtoList" :key="todo.id" @click="emits('moveTodo', todo.id)">
+      <li v-for="todo in data.dtoList" :key="todo.id" @click="handleClickTodo(todo.id)">
         {{ todo }}
       </li>
     </ul>
@@ -12,7 +12,7 @@
 
   <ul class="paging">
     <li v-for="page in data.pageNumList">
-      <v-btn @click="()=>emits('movePage', page) ">{{ page }}</v-btn>
+      <v-btn @click="handleClickPageNum(page) ">{{ page }}</v-btn>
     </li>
   </ul>
 
@@ -24,6 +24,14 @@ import authAxios from "@/util/authAxios";
 
 const props = defineProps(['query'])
 const emits = defineEmits(['movePage', 'moveTodo'])
+
+const handleClickPageNum = (page) => {
+  emits('movePage', page)
+}
+
+const handleClickTodo = (id) => {
+  emits('moveTodo', id)
+}
 
 const res = await authAxios.get(`http://localhost:8080/api/todos/list?page=${props.query.page || 1}`)
 const data = res.data
